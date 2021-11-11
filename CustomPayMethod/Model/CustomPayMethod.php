065e1b42d4ffdd090b3f2a1517371727421b9842
ShashiKant Saini <shashikantsaini.sk@gmail.com>
2021-11-11 17:05:25 +0530

    Create Custom Payment Method on Checkout Magento 2

---
+++ CustomPayMethod/Model/CustomPayMethod.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Bluethink\CustomPayMethod\Model;
+
+class CustomPayMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{    
+    protected $_code = 'custompaymethod';
+}
