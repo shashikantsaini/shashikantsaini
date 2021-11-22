b01d0bc01e578d6b671cd84e3927a110c6d58213
ShashiKant Saini <shashikantsaini.sk@gmail.com>
2021-11-22 20:27:28 +0530

    Made changes in Model file Custompay method

--- CustomPayMethod/Model/CustomPayMethod.php
+++ CustomPayMethod/Model/CustomPayMethod.php
@@ -5,4 +5,11 @@ namespace Bluethink\CustomPayMethod\Model;
 class CustomPayMethod extends \Magento\Payment\Model\Method\AbstractMethod
 {    
     protected $_code = 'custompaymethod';
+/**
+     * Payment Method feature
+     *
+     * @var bool
+     */
+    protected $_canAuthorize = true;
+
 }
